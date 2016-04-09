6e61b35b71421f31f1bd5bf0f5677e3eee850d9e
thebatoust <thebatoust@gmail.com>
2016-04-09 12:18:09 -0400

    Scene début combat

---
+++ Assets/LoadSceneAfterAnimation.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneAfterAnimation : MonoBehaviour {
+
+    public float animationDuration;
+    public int nextSceneIndex;
+
+    bool overrideLaunch = false;
+
+    void Start()
+    {
+        StartCoroutine(LaunchNextSceneAfter());
+        StartCoroutine(OverrideLaunch());
+    }
+
+    void Update () {
+	    if(Input.GetButton("P1Action"))
+        {
+            overrideLaunch = true;
+        }
+	}
+
+    void LaunchNextScene()
+    {
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    IEnumerator LaunchNextSceneAfter()
+    {
+        yield return new WaitForSeconds(animationDuration);
+        if(!overrideLaunch)
+            LaunchNextScene();
+    }
+
+    IEnumerator OverrideLaunch()
+    {
+        while (overrideLaunch == false)
+            yield return null;
+        LaunchNextScene();
+    }
+}
